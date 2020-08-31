5b75b001a3e4f519fb1aeb3b0f42065e8764395e
LnX <in@lnx.rip>
2020-08-31 17:23:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,99 @@
-zgtfhjf
+import discord
+import random
+import string
+import asyncio
+import os
+import json
+from colorama import Fore
+
+from discord.ext import (
+    commands,
+    tasks
+)
+
+client = discord.Client()
+client = commands.Bot(
+    command_prefix="!",
+    self_bot=True
+)
+client.remove_command('help')
+
+with open('config.json') as f:
+    config = json.load(f)
+    
+token = config.get("token")
+
+def scale(time):
+    defined = 60
+    for unit in ["m", "h"]:
+        if time < defined:
+            return f"{time:.2f}{unit}"
+        time /= defined
+
+def Init():
+    if config.get('token') == "token-here":
+        os.system('cls')
+        print(f"\n\n{Fore.WHITE}[ {Fore.RED}E {Fore.WHITE}] {Fore.LIGHTBLACK_EX}You didnt put your token in the config.json file\n\n"+Fore.RESET)
+        exit()
+    else:
+        token = config.get('token')
+        try:
+            client.run(token, bot=False, reconnect=True)
+            os.system(f'Discord LevelUpBot')
+        except discord.errors.LoginFailure:
+            print(f"\n\n{Fore.WHITE}[ {Fore.RED}E {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Token is invalid\n\n"+Fore.RESET)
+            exit()
+
+def rnd1(length):
+    return ''.join(random.choice(string.ascii_letters) for i in range(length))
+def rnd2(length):
+    return ''.join(random.choice(string.ascii_lowercase) for i in range(length))
+
+os.system('cls')
+print(f"{Fore.WHITE}[ {Fore.CYAN}§ {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Discord LevelUp Bot made by {Fore.WHITE}LnX{Fore.LIGHTBLACK_EX} | Licensed under {Fore.WHITE}MIT {Fore.LIGHTBLACK_EX}License")
+print(f"{Fore.WHITE}[ {Fore.CYAN}§ {Fore.WHITE}] {Fore.LIGHTBLACK_EX}You can follow me on Github: {Fore.WHITE}https://github.com/lnxcz\n")
+
+print(f"{Fore.WHITE}[ {Fore.GREEN}+ {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Bot is ready!")
+print(f"{Fore.WHITE}[ {Fore.YELLOW}? {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Write {Fore.WHITE}!levelup <number of messages>{Fore.LIGHTBLACK_EX} to start level up\n")
+
+
+@client.command()
+async def levelup(ctx,amount: int):
+    await ctx.message.delete()
+    msgsend = amount
+    print(f"\n{Fore.WHITE}[ {Fore.YELLOW}? {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Sending {Fore.WHITE}{msgsend} {Fore.LIGHTBLACK_EX}messages\n{Fore.WHITE}[ {Fore.YELLOW}? {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Estimated Time: {Fore.WHITE}{scale(msgsend)}\n")
+    while msgsend > 0:
+        try:
+            msgsend -= 1
+            print(f"{Fore.WHITE}[ {Fore.GREEN}+ {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Message sent! | Messages left to send: {Fore.WHITE}{msgsend} {Fore.LIGHTBLACK_EX}| Estimated Time: {Fore.WHITE}{scale(msgsend)}")
+            if msgsend == 0:
+                print(f"\n{Fore.WHITE}[ {Fore.GREEN}+ {Fore.WHITE}] {Fore.LIGHTBLACK_EX}All messages was sent")
+            output = rnd1(5) + " " + rnd2(5) + "-" + rnd2(5) + " " + rnd2(5) + "-" + rnd2(5) + " " + rnd1(5)
+            await ctx.send(output)
+        except:
+            print(f"{Fore.WHITE}[ {Fore.RED}- {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Cannot send message {Fore.WHITE}#{msgsend}")
+            pass
+        await asyncio.sleep(1)
+        async for message in ctx.message.channel.history(limit=1).filter(lambda m: m.author == client.user).map(lambda m: m):
+            try:
+                await message.delete()
+            except:
+                print(f"{Fore.WHITE}[ {Fore.RED}- {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Cannot delete message {Fore.WHITE}#{msgsend}")
+                pass
+        await asyncio.sleep(60)
+    return
+
+
+@client.event
+async def on_command_error(ctx, error):
+    error_str = str(error)
+    error = getattr(error, 'original', error)
+    if isinstance(error, commands.CommandNotFound):
+        return
+    elif isinstance(error, discord.errors.Forbidden):
+        print(f"{Fore.WHITE}[ {Fore.RED}E {Fore.WHITE}] {Fore.LIGHTBLACK_EX}Discord error: {error}"+Fore.RESET)    
+    else:
+        print(f"{Fore.WHITE}[ {Fore.RED}E {Fore.WHITE}] {Fore.LIGHTBLACK_EX}{error_str}"+Fore.RESET)
+
+
+Init()
